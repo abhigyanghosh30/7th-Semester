5e457e8041924025e76ea9727040ccc7becfbf13
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2020-10-11 04:33:18 +0530

    IRE has just too many files

--- SSP/A5/ECE449_A5_20171089/cbq/q2.m
+++ SSP/A5/ECE449_A5_20171089/cbq/q2.m
@@ -18,10 +18,10 @@ x = sig(2:n)-sig(1:n-1)
 % y_2 = cat(1,y_1(1:2),y_2)
 % y = y_2 - sum(y_2,1)/n
 
-y_1 = cumtrapz(cumtrapz(x))
-y_2 = cumtrapz(cumtrapz(x))
+% y_1 = cumtrapz(cumtrapz(x))
+y_2 = cumtrapz(cumtrapz(cumtrapz(cumtrapz(x))))
 
-m = floor(15*Fs/1000)
+m = floor(5*Fs/1000)
 
 y = []
 k = 1
@@ -30,4 +30,4 @@ for i=1+m:length(y_2)-m
     k=k+1
 end
 
-subplot(3,1,3);plot([1:length(y)]/Fs,y);title('15ms mean subtracted ZFF')
+subplot(3,1,3);plot([1:length(y)]/Fs,y);title('5ms mean subtracted ZFF')
