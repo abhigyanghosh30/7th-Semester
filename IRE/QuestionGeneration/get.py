b87110a9912d5aa4b4d6b756a7513d0d967595c7
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2020-10-19 04:16:45 +0530

    Added a test file

---
+++ IRE/QuestionGeneration/get.py
@@ -0,0 +1,10 @@
+import re
+with open('questions.txt') as f:
+    total = 0
+    count = 0
+    for line in f.readlines():
+        line = line.strip()
+        stat = re.split('\D', line)
+        count += 1
+        total += float(stat[-2]+stat[-1])/pow(10, len(stat[-1]))
+    print(count, total/count)
