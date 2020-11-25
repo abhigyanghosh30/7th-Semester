0ec22e2075f6e897642c45edadf17e2bafc1b316
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2020-11-25 22:33:03 +0530

    light one done too

--- DA-1/FINAL_PROJECT/Light/submission.py
+++ DA-1/FINAL_PROJECT/Light/submission.py
@@ -1,12 +1,11 @@
 import os
 import pandas as pd
 import numpy as np
-from sklearn.decomposition import PCA
-from sklearn.ensemble import RandomForestClassifier, StackingClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
+from sklearn.preprocessing import StandardScaler
 
 # Train and test data paths will be available as env variables during evaluation
 TRAIN_DATA_PATH = os.getenv("TRAIN_DATA_PATH", default='./train.csv')
@@ -16,20 +15,19 @@ TEST_DATA_PATH = os.getenv("TEST_DATA_PATH", default='./test.csv')
 train_data = pd.read_csv(TRAIN_DATA_PATH)
 X_train, y_train = train_data.iloc[:, :-1], train_data.iloc[:, -1]
 # X_train['average'] = X_train.mean(axis=1)
-
-
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
 # Train the model
-# classifier = SVC(gamma='auto')
-models = [('rf', RandomForestClassifier(n_estimators=1000, random_state=42)),
-          ('svr', DecisionTreeClassifier())]
+# classifier = SVC(kernel='poly', degree=10, gamma='auto')
 
-classifier = StackingClassifier(
-    estimators=models, final_estimator=SVC())
+classifier = RandomForestClassifier(n_estimators=1000)
 classifier.fit(X_train, y_train)
 
 # Predict on the test set
 test_data = pd.read_csv(TEST_DATA_PATH)
 # test_data['average'] = test_data.mean(axis=1)
+test_data = sc.fit_transform(test_data)
+
 submission = classifier.predict(test_data)
 submission = pd.DataFrame(submission)
 
